2cacb4750882f11953385609a5845ef55eb92f28
sergio-teruel <sergio.teruel@tecnativa.com>
2021-06-25 20:00:17 +0200

    [IMP] stock_barcodes: Add guided mode and restyling.

--- stock_barcodes/models/stock_picking.py
+++ stock_barcodes/models/stock_picking.py
@@ -7,17 +7,23 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def action_barcode_scan(self):
-        out_picking = self.picking_type_code == "outgoing"
-        location = self.location_id if out_picking else self.location_dest_id
+        option_group = self.picking_type_id.barcode_option_group_id
+        vals = {
+            "picking_id": self.id,
+            "res_model_id": self.env.ref("stock.model_stock_picking").id,
+            "res_id": self.id,
+            "picking_type_code": self.picking_type_code,
+            "option_group_id": option_group.id,
+            "picking_mode": "picking",
+        }
+        if option_group.get_option_value("location_id", "filled_default"):
+            out_picking = self.picking_type_code == "outgoing"
+            location = self.location_id if out_picking else self.location_dest_id
+            vals["location_id"] = location.id
+        wiz = self.env["wiz.stock.barcodes.read.picking"].create(vals)
+        wiz.determine_todo_action()
         action = self.env.ref(
             "stock_barcodes.action_stock_barcodes_read_picking"
         ).read()[0]
-        action["context"] = {
-            "default_location_id": location.id,
-            "default_partner_id": self.partner_id.id,
-            "default_picking_id": self.id,
-            "default_res_model_id": self.env.ref("stock.model_stock_picking").id,
-            "default_res_id": self.id,
-            "default_picking_type_code": self.picking_type_code,
-        }
+        action["res_id"] = wiz.id
         return action
--- stock_barcodes/models/stock_picking_type.py
+++ stock_barcodes/models/stock_picking_type.py
@@ -1,22 +1,35 @@
 # Copyright 2019 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import fields, models
 
 
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
+    barcode_option_group_id = fields.Many2one(
+        comodel_name="stock.barcodes.option.group"
+    )
+
     def action_barcode_scan(self):
+        vals = {
+            "res_model_id": self.env.ref("stock.model_stock_picking_type").id,
+            "res_id": self.id,
+            "picking_type_code": self.code,
+            "option_group_id": self.barcode_option_group_id.id,
+            "picking_mode": "picking",
+        }
+        if self.barcode_option_group_id.get_option_value(
+            "location_id", "filled_default"
+        ):
+            if self.code == "incoming":
+                location = self.default_location_dest_id
+            elif self.code in ["outgoing", "internal"]:
+                location = self.default_location_src_id
+            vals["location_id"] = location.id
+        wiz = self.env["wiz.stock.barcodes.read.picking"].create(vals)
+        wiz.determine_todo_action()
         action = self.env.ref(
             "stock_barcodes.action_stock_barcodes_read_picking"
         ).read()[0]
-        action["context"] = {
-            "default_res_model_id": self.env.ref("stock.model_stock_picking_type").id,
-            "default_res_id": self.id,
-            "default_picking_type_code": self.code,
-        }
-        if self.code == "incoming":
-            action["context"]["default_location_id"] = self.default_location_dest_id.id
-        elif self.code in ["outgoing", "internal"]:
-            action["context"]["default_location_id"] = self.default_location_src_id.id
+        action["res_id"] = wiz.id
         return action
